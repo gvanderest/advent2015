82cd63c78ba84b62d351a00f90dc90559b0c0068
Guillaume VanderEst <gvanderest@gmail.com>
2021-11-23 14:28:17 -0800

    Day 1 completed.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,38 @@
+use std::fs;
+
+fn parse_floor_part_1(input: String) -> i32 {
+    let mut floor = 0;
+    for c in input.chars() {
+        floor = floor
+            + match c {
+                '(' => 1,
+                ')' => -1,
+                _ => 0,
+            }
+    }
+    floor
+}
+
+fn parse_floor_part_2(input: String) -> usize {
+    let mut floor: i32 = 0;
+    for (index, c) in input.chars().enumerate() {
+        floor = floor
+            + match c {
+                '(' => 1,
+                ')' => -1,
+                _ => 0,
+            };
+        if floor < 0 {
+            return index + 1;
+        }
+    }
+    panic!("Didn't find a floor.")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input.txt").expect("Couldn't read the input.");
+
+    println!("Part 1: {}", parse_floor_part_1(input.clone()));
+
+    println!("Part 2: {}", parse_floor_part_2(input.clone()));
 }
