303271a75a20b8420d9d5e9ee409f3fc8a3faf5a
Guillaume VanderEst <guillest@amazon.com>
2024-12-02 10:34:57 -0800

    Create a template for starting new days

---
+++ template/main.rs
@@ -0,0 +1,32 @@
+use std::fs;
+
+fn part1_solve(input: &str) -> u64 {
+    0
+}
+
+fn part2_solve(input: &str) -> u64 {
+    0
+}
+
+fn main() {
+    let input = fs::read_to_string("input.txt").unwrap();
+    println!("Part 1: {}", part1_solve(&input));
+    println!("Part 2: {}", part2_solve(&input));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let input = fs::read_to_string("example.txt").unwrap();
+        assert_eq!(1, part1_solve(&input));
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = fs::read_to_string("example.txt").unwrap();
+        assert_eq!(1, part2_solve(&input));
+    }
+}
